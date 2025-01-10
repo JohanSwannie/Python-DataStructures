8d509b47543dc7f4adc8434a551b4bc59ad9acc0
Johan Swan <jswannieman@gmail.com>
2025-01-10 21:30:24 +1300

    Commit pythonDictionaries PY Component

---
+++ pythonDictionaries.py
@@ -0,0 +1,11 @@
+
+# ? #####################
+# ? PYTHON DICTIONARIES #
+# ? #####################
+
+dict1 = {"name": 'Jan', "age": 27, "occupation": "Software Developer"}
+
+print(dict1.keys())
+print(dict1.values())
+print(dict1.items())
+
